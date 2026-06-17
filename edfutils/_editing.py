35734ba6f61d594d9fe7ad99d8de5035e6bcc39f
Peter Brotherwood <p.brotherwood@outlook.com>
2026-06-16 22:44:39 -0400

    Adjusted how trigger samples are computed to avoid oncorrect insertion when linear drift occurs

--- edfutils/_editing.py
+++ edfutils/_editing.py
@@ -220,7 +220,7 @@ class _EditingMixin:
         indices = np.searchsorted(self.log.onset.to_numpy(), times)
 
         last_event = self.log.iloc[indices-1]
-        tdiffs = [events.onset.to_numpy() - last_event.onset.to_numpy()]
+        tdiffs = events.onset.to_numpy() - last_event.onset.to_numpy()
         insert_samples = [np.array(samples) + int(tdiffs[idx]*self.sfreq) for idx, samples in enumerate(last_event.trigger_idcs.to_list())]  
 
         #last_event = np.max(np.where(~self.log['trigger_idcs'].isna()))
